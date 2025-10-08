889be8ea08ccbe65179e44f76d899d779cbaec41
jan.racek <janra2323@gmail.com>
2025-10-08 14:02:36 +0200

    bankomat

--- src/gui/browser/CardMenu.java
+++ src/gui/browser/CardMenu.java
@@ -9,7 +9,7 @@ public class CardMenu extends JFrame {
     JLabel fullName, salary, shoeSize;
     ImageIcon avatar;
     JPanel lowerMenu;
-    JButton next, previous;
+    JButton next, previous, first, last;
     int cursor;
 
     CardMenu(){
@@ -47,29 +47,81 @@ public class CardMenu extends JFrame {
         salary.setText("Salary: " + MainMenu.personData.get(cursor).salary);
         previous = new JButton("Previous");
         next = new JButton("Next");
+        first = new JButton("First");
+        first.addActionListener(e->{
+            cursor=0;
+            previous.setEnabled(false);
+            next.setEnabled(true);
+            first.setEnabled(false);
+            draw(cursor);
+        });
+        last = new JButton("Last");
+        lowerMenu.add(first);
         lowerMenu.add(previous);
+        previous.setEnabled(false);
+        first.setEnabled(false);
         previous.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //dalsi kod
+                if (cursor<=0){
+                    previous.setEnabled(false);
+                    first.setEnabled(false);
+                }else {
+                    first.setEnabled(true);
+                    previous.setEnabled(true);
+                    next.setEnabled(true);
+                    cursor--;
+                    draw(cursor);
+                    if (cursor<=0){
+                        previous.setEnabled(false);
+                        first.setEnabled(false);
+                    }
+
+                }
+
             }
         });
+
+
+
         lowerMenu.add(shoeSize);
         lowerMenu.add(salary);
         next.addActionListener(e -> {
-            cursor++;
-            avatar = new ImageIcon(MainMenu.personData.get(cursor).avatar.getImage().getScaledInstance(500,450, Image.SCALE_DEFAULT));
-            fullName.setText("Full name: " + MainMenu.personData.get(cursor).name + " " + MainMenu.personData.get(cursor).surname);
-            fullName.setIcon(avatar);
+            if (cursor>=MainMenu.personData.size()-1){
+                next.setEnabled(false);
+            } else {
+                previous.setEnabled(true);
+                first.setEnabled(true);
+                cursor++;
+                draw(cursor);
+                if (cursor>=MainMenu.personData.size()-1){
+                    next.setEnabled(false);
+                }
+            }
 
-            shoeSize.setText("Shoe size: " + MainMenu.personData.get(cursor).shoeSize);
-            salary.setText("Salary: " + MainMenu.personData.get(cursor).salary);
+
         });
-
+        last.addActionListener(e->{
+            cursor=MainMenu.personData.size()-1;
+            previous.setEnabled(true);
+            next.setEnabled(false);
+            previous.setEnabled(true);
+            first.setEnabled(true);
+            draw(cursor);
+        });
         lowerMenu.add(next);
+        lowerMenu.add(last);
 
         this.add(lowerMenu, BorderLayout.SOUTH);
         this.pack();
+    }
+    void draw(int cursor){
+        avatar = new ImageIcon(MainMenu.personData.get(cursor).avatar.getImage().getScaledInstance(500,450, Image.SCALE_DEFAULT));
+        fullName.setText("Full name: " + MainMenu.personData.get(cursor).name + " " + MainMenu.personData.get(cursor).surname);
+        fullName.setIcon(avatar);
+
+        shoeSize.setText("Shoe size: " + MainMenu.personData.get(cursor).shoeSize);
+        salary.setText("Salary: " + MainMenu.personData.get(cursor).salary);
     }
 
 }
